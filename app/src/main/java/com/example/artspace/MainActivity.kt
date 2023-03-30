a8da20237d44289a988984f03bd4f5e2df236c34
mdyus <mohammedyusufmujawar92@gmail.com>
2023-03-30 13:56:39 +0530

    Added functionality to the buttons and made some improvements to the UI.

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -42,21 +45,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtSpaceScreen() {
-    ArtSpaceImageWithText(
-        artWorkWall = R.drawable.artwall_1,
-        artWorkTitle = R.string.image_1_title,
-        artWorkArtist = R.string.image_1_artist,
-        artWorkYear = R.string.image_1_year
-    )
+    var result by remember { mutableStateOf(1) }
+    when (result) {
+        1 -> ArtSpaceImageWithText(
+            artWorkWall = R.drawable.artwall_1, artWorkTitle = R.string.image_1_title,
+            artWorkArtist = R.string.image_1_artist, artWorkYear = R.string.image_1_year
+        )
+        2 -> ArtSpaceImageWithText(
+            artWorkWall = R.drawable.artwall_2, artWorkTitle = R.string.image_2_title,
+            artWorkArtist = R.string.image_2_artist, artWorkYear = R.string.image_2_year
+        )
+        3 -> ArtSpaceImageWithText(
+            artWorkWall = R.drawable.artwall_3, artWorkTitle = R.string.image_3_title,
+            artWorkArtist = R.string.image_3_artist, artWorkYear = R.string.image_3_year
+        )
+        else -> ArtSpaceImageWithText(
+            artWorkWall = R.drawable.artwall_4, artWorkTitle = R.string.image_4_title,
+            artWorkArtist = R.string.image_4_artist, artWorkYear = R.string.image_4_year
+        )
+    }
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Bottom,
         modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
             .wrapContentHeight(Alignment.Bottom)
             .wrapContentWidth(Alignment.CenterHorizontally)
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                if (result > 1) {
+                    result -= 1
+                } else {
+                    result = (1..4).random()
+                }
+            },
             modifier = Modifier
                 .padding(15.dp)
                 .wrapContentWidth(Alignment.Start)
@@ -65,7 +89,13 @@ fun ArtSpaceScreen() {
         }
         Spacer(modifier = Modifier.width(30.dp))
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                if (result <= 4) {
+                    result += 1
+                } else {
+                    result = 1
+                }
+            },
             modifier = Modifier
                 .padding(15.dp)
                 .wrapContentWidth(Alignment.End)
@@ -86,16 +116,23 @@ fun ArtSpaceImageWithText(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier
+            .padding(10.dp)
             .fillMaxSize()
             .wrapContentSize(Alignment.Center)
+            .border(
+                BorderStroke(4.dp, Color.Gray),
+                RectangleShape
+            )
+            .shadow(elevation = 5.dp)
     ) {
         Image(
             painter = painterResource(id = artWorkWall),
             contentDescription = null,
-            contentScale = ContentScale.Inside,
+            contentScale = ContentScale.Fit,
             modifier = Modifier
-                .size(350.dp)
-                .wrapContentSize(Alignment.Center)
+                .height(350.dp)
+                .width(300.dp)
+                .padding(20.dp)
         )
     }
     Spacer(modifier = Modifier.height(10.dp))
@@ -104,39 +141,36 @@ fun ArtSpaceImageWithText(
         verticalArrangement = Arrangement.Bottom,
         modifier = Modifier
             .padding(bottom = 100.dp)
-            .shadow(shape = RectangleShape, elevation = 2.dp)
             .fillMaxWidth()
+            .fillMaxSize()
             .wrapContentWidth(Alignment.CenterHorizontally)
+            .wrapContentSize(Alignment.BottomCenter)
+            .shadow(elevation = 5.dp)
     ) {
         Text(
             text = stringResource(id = artWorkTitle),
-            fontSize = 24.sp,
+            fontSize = 36.sp,
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(5.dp)
         )
         Spacer(modifier = Modifier.height(5.dp))
-        Text(
-            text = stringResource(id = artWorkArtist),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = stringResource(id = artWorkYear),
-            fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally)
-        )
+                .padding(10.dp)
+        ) {
+            Text(
+                text = stringResource(id = artWorkArtist),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(end = 5.dp)
+            )
+            Text(
+                text = stringResource(id = artWorkYear),
+                fontSize = 24.sp
+            )
+        }
     }
 }
-
-/*fun performAction() {
-
-}*/
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
